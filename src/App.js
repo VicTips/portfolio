3ad7faeff198eb0cbe74275a17b525d6711fc230
victips <torvicarias@gmail.com>
2022-08-16 07:46:35 -0500

    Create footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 import Skills from "./components/Skills/Skills";
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <NavBar />
       <Skills />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark">
+      <div className="container text-white text-center py-2">
+        Copyright © 2022. All Rights Reserved | Designed by Victor Arias
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
