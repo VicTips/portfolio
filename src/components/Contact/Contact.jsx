9283d2b39acad2adf34d6fa76938488522eaad16
victips <torvicarias@gmail.com>
2022-08-16 19:05:41 -0500

    Add environment variables

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,9 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const YOUR_SERVICE_ID = process.env.REACT_APP_YOUR_SERVICE_ID;
+  const YOUR_TEMPLATE_ID = process.env.REACT_APP_YOUR_TEMPLATE_ID;
+  const YOUR_PUBLIC_KEY = process.env.REACT_APP_YOUR_PUBLIC_KEY;
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -9,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_974u6zp",
-        "template_ua1vn4i",
+        YOUR_SERVICE_ID,
+        YOUR_TEMPLATE_ID,
         form.current,
-        "yzABc7McEglpmtGXd"
+        YOUR_PUBLIC_KEY
       )
       .then(
         (result) => {
@@ -26,31 +29,34 @@ const Contact = () => {
       );
   };
   return (
-    <div className="text-secondary py-5" style={{ width: "25rem" }}>
+    <div className="text-secondary py-4" style={{ width: "30rem" }}>
+      <div className="fs-1 fw-bold mb-3">Contact me</div>
       <form ref={form} onSubmit={sendEmail}>
-        <div className="mb-2">
-          <label className="form-label">Name</label>
+        <div className="mb-2 d-flex gap-2">
           <input
             type="text"
             name="user_name"
-            className="form-control"
-            placeholder="Jane Doe"
+            className="form-control bg-dark text-light"
+            placeholder="Name"
           />
-        </div>
-        <div className="mb-2">
-          <label className="form-label">Email</label>
           <input
             type="email"
             name="user_email"
-            className="form-control"
-            placeholder="jane@example.com"
+            className="form-control bg-dark text-light"
+            placeholder="Email"
           />
         </div>
         <div className="mb-2">
-          <label className="form-label">Message</label>
-          <textarea name="message" className="form-control" />
+          <textarea
+            name="message"
+            className="form-control bg-dark text-light"
+            placeholder="Message"
+            rows={4}
+          />
         </div>
-        <input type="submit" value="Send" className="btn btn-light" />
+        <div className="mt-3 d-flex justify-content-end">
+          <input type="submit" value="Send" className="btn btn-light" />
+        </div>
       </form>
     </div>
   );
